d5ebc139a84c599cb53bb73cb0501367cc4e55cf
Allan Rafael <allanrafaelfo@gmail.com>
2021-03-14 00:33:07 -0300

    Adiciona app customers as configurações

--- django_react/settings.py
+++ django_react/settings.py
@@ -16,7 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'customers'
 ]
 
 MIDDLEWARE = [
