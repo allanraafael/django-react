9bf13f337ce44a69db382cadf3d3c73fd922937c
Allan Rafael <allanrafaelfo@gmail.com>
2021-03-14 14:18:40 -0300

    Cria as rotas para as views

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from backend.apps.customers import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/customers/', views.customers_list),
+    path('api/customers/<int:pk>/', views.customers_detail),
 ]
