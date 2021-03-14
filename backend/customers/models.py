321643b4821fc2076adfa41a2bee3f3579b918ad
Allan Rafael <allanrafaelfo@gmail.com>
2021-03-14 01:49:34 -0300

    Adiciona model Customer

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField("First name", max_length=255)
+    last_name = models.CharField("Last name", max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address =  models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
