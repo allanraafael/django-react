f10836acf3e9b30c70469646f1816ea3892e1856
Allan Rafael <allanrafaelfo@gmail.com>
2021-03-14 23:04:07 -0300

    Modifica App.js para bootstrap4 com a listagem e criação de clientes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+
+import  CustomersList from './apps/costumers/componets/CustomersList'
+import  CustomerCreateUpdate  from './apps/costumers/componets/CustomerCreateUpdate'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const BaseLayout = () => (
+    <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+            <a className="navbar-brand" href="#">Django React Demo</a>
+
+            <button className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-item nav-link" href="/">CUSTOMERS</a>
+                    <a className="nav-item nav-link" href="/customer">CREATE CUSTOMER</a>
+                </div>
+            </div>
+
+        </nav>
+
+        <div className="content">
+            <Route path="/" exact component={CustomersList} />
+            <Route path="/customer/:pk"  component={CustomerCreateUpdate} />
+            <Route path="/customer/" exact component={CustomerCreateUpdate} />
+
+        </div>
+
     </div>
-  );
+)
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <BaseLayout/>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
