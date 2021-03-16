21c2237da01c1e0ff513b29ec7b24ec610f097ed
Allan Rafael <allanrafaelfo@gmail.com>
2021-03-15 22:30:34 -0300

    Adiciona um novo domínio a lista de urls permitidas da API-backend pelo CORS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -34,7 +34,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
-    'https://localhost:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 )
 
 ROOT_URLCONF = 'backend.core.urls'
